8d3fa37b832c788b7cbd4c7d7fc0a817b9b5a3ce
Jolomimeb <111466708+Jolomimeb@users.noreply.github.com>
2023-07-19 20:14:33 -0400

    testFile.cpp

--- testFile.cpp
+++ testFile.cpp
@@ -1,15 +1,3 @@
-/*****************************************
-** File:    mytest.cpp
-** Project: CMSC 341 Project 2, Spring 2023
-** Author:  Oritsejolomisan Mebaghanje
-** Date:    03/28/23
-** E-mail:  xz94254@umbc.edu
-**
-** This file contains the program for mytest.cpp.
-**
-**
-***********************************************/
-
 #include "show.h"
 using namespace std;
 
